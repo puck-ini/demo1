c7e86f7f401832f829870591d85ffd1494176555
my name <email@qq.com>
2019-09-25 16:46:20 +0800

    secutiry 9.25 2

--- src/main/java/com/test/demo1/config/SecurityConfig.java
+++ src/main/java/com/test/demo1/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.test.demo1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,5 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/test/demo1/config/UDSConfig.java
@@ -0,0 +1,30 @@
+package com.test.demo1.config;
+
+
+import com.test.demo1.entity.TestUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UDSConfig implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        TestUser testUser = new TestUser();
+        testUser.setUsername(username);
+        testUser.setPassword(passwordEncoder.encode("123465"));
+        System.out.println(testUser.getPassword());
+        return new User(username,testUser.getPassword(),testUser.isEnabled(),
+                testUser.isAccountNonExpired(),testUser.isCredentialsNonExpired(),
+                testUser.isAccountNonLocked(), AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
